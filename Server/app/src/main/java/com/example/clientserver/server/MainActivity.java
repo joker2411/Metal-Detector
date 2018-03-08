a316cf05497d109b965f01218155c886f1765bce
AMBUJ MISHRA <ambuj.mishra.2k12@gmail.com>
2018-03-08 14:17:53 +0530

    Server side of the project
    This is the server side of our complete projects. It shows the ip address and port no. on which it is running. It collects the data sent by various clients and if the magnetic intensity is greater than a certain thresold value than a siren starts beeping.

---
+++ Server/app/src/main/java/com/example/clientserver/server/MainActivity.java
@@ -0,0 +1,190 @@
+package com.example.clientserver.server;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.io.*;
+import java.net.ServerSocket;
+import java.net.*;
+import java.util.*;
+public class MainActivity extends AppCompatActivity {
+
+    TextView info, infoip, msg;
+    String message = "";
+    ServerSocket serverSocket;
+    String[] Result;
+    Double maxValue;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        info = (TextView) findViewById(R.id.info);
+        infoip = (TextView) findViewById(R.id.infoip);
+        msg = (TextView) findViewById(R.id.msg);
+
+        infoip.setText(getIpAddress());
+
+        Thread socketServerThread = new Thread(new SocketServerThread());
+        socketServerThread.start();
+
+    }
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+
+        if (serverSocket != null) {
+            try {
+                serverSocket.close();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public class SocketServerThread extends Thread {
+
+        static final int SocketServerPORT = 8080;
+        int count = 0;
+
+        @Override
+        public void run() {
+            Socket socket = null;
+            DataInputStream dataInputStream = null;
+            DataOutputStream dataOutputStream = null;
+
+            try {
+                serverSocket = new ServerSocket(SocketServerPORT);
+                MainActivity.this.runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        info.setText("Port: "
+                                + serverSocket.getLocalPort());
+                    }
+                });
+
+                while (true) {
+                    socket = serverSocket.accept();
+                    dataInputStream = new DataInputStream(
+                            socket.getInputStream());
+                    dataOutputStream = new DataOutputStream(
+                            socket.getOutputStream());
+
+                    String messageFromClient = "";
+
+
+                    messageFromClient = dataInputStream.readUTF();
+                    Result = messageFromClient.split(" ");
+                    maxValue = Double.parseDouble(Result[0]);
+                    count++;
+                    message += "#" + count + " from " + socket.getInetAddress()
+                            + ":" + socket.getPort() + "\n"
+                            + "Msg from client: " + messageFromClient + "\n";
+
+                    MainActivity.this.runOnUiThread(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            msg.setText(message);
+
+                        }
+
+                    });
+                    playMedia();
+
+                    String msgReply = "Hello from Android, you are #" + count;
+                    dataOutputStream.writeUTF(msgReply);
+
+                }
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+                final String errMsg = e.toString();
+                MainActivity.this.runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        msg.setText(errMsg);
+                    }
+                });
+
+            } finally {
+                if (socket != null) {
+                    try {
+                        socket.close();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+
+                if (dataInputStream != null) {
+                    try {
+                        dataInputStream.close();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+
+                if (dataOutputStream != null) {
+                    try {
+                        dataOutputStream.close();
+                    } catch (IOException e) {
+                        // TODO Auto-generated catch block
+                        e.printStackTrace();
+                    }
+                }
+
+            }
+        }
+
+
+    }
+    public void playMedia()
+    {
+        if((maxValue>800.0)&&Result[1].equals("μT"))
+        {
+            MediaPlayer MP = MediaPlayer.create(this,R.raw.beep);
+            MP.start();
+        }
+    }
+
+    public String getIpAddress() {
+        String ip = "";
+        try {
+            Enumeration<NetworkInterface> enumNetworkInterfaces = NetworkInterface
+                    .getNetworkInterfaces();
+            while (enumNetworkInterfaces.hasMoreElements()) {
+                NetworkInterface networkInterface = enumNetworkInterfaces
+                        .nextElement();
+                Enumeration<InetAddress> enumInetAddress = networkInterface
+                        .getInetAddresses();
+                while (enumInetAddress.hasMoreElements()) {
+                    InetAddress inetAddress = enumInetAddress.nextElement();
+
+                    if (inetAddress.isSiteLocalAddress()) {
+                        ip += "IP Address: "
+                                + inetAddress.getHostAddress() + "\n";
+                    }
+
+                }
+
+            }
+
+        } catch (SocketException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+            ip += "Something Wrong! " + e.toString() + "\n";
+        }
+
+        return ip;
+    }
+
+}
